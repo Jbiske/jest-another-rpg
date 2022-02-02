36d5906f53fa55d908c3c09b45bc30cda6095523
Josh <biscuitemail2@gmail.com>
2022-02-02 15:01:15 -0500

    player

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -4,3 +4,12 @@ const checkIfEqual = require('../lib/random.js');
 test('checks if 10 is equal to 10', () => {
     expect(checkIfEqual(10, 10)).toBe(true);
 })
+
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
